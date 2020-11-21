8dc7b771022ca4bc2f397ba2746b26dc9e9ff070
”naiteru3” <naiteru33@yahoo.co.jp>
2020-11-21 09:03:36 +0000

    課題　php 02

---
+++ practice.php
@@ -0,0 +1,39 @@
+<?php
+
+$a = 3;
+$b = 7;
+echo $a + $b ;
+echo "\n";
+
+$array_month = ["january","february","march","april","may","june","july","august","september","october","november","decermber"];
+echo $array_month[7];
+echo "\n";
+
+$hello ="Hello,";
+$name = "naito yoshiteru";
+$world = "'s World!";
+echo $hello . $name . $world ;
+echo "\n";
+
+$tech_boost ="tech";
+$tech_boost .="boost";
+echo $tech_boost;
+echo "\n";
+
+
+$a2018_calendar = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月",
+];
+echo $a2018_calendar["December"];
+echo "\n";
